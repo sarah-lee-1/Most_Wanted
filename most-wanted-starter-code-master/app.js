57235dde860596f66e7622e86f438bc525683d7e
sarah-lee-1 <93781113+sarah-lee-1@users.noreply.github.com>
2021-12-22 10:56:10 -0600

    Add code to return first name in Alert.

--- most-wanted-starter-code-master/app.js
+++ most-wanted-starter-code-master/app.js
@@ -81,8 +81,9 @@ function searchByName(people){
     }
   })
   // TODO: find the person single person object using the name they entered.
-  
-  return foundPerson;
+  foundPerson(id)
+
+  return foundPerson(firstName, lastName);
 }
 
 //unfinished function to search through an array of people to find matching eye colors. Use searchByName as reference.
