4937a0a06daf98dc9ded9c615e3f16da2a23e5f1
JaredMartin0351 <jaredmartin0351@gmail.com>
2021-12-23 11:38:15 -0600

    Update app.js

--- most-wanted-starter-code-master/app.js
+++ most-wanted-starter-code-master/app.js
@@ -36,7 +36,7 @@ function mainMenu(person, people){
     return app(people); // restart
   }
 
-  let displayOption = promptFor("Found" + person[0].firstName + " " + person[0].lastName + " " + "Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid);
+  let displayOption = promptFor("Found " + person[0].firstName + " " + person[0].lastName + " " + "Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'", autoValid);
 
   switch(displayOption){
     case "info":
